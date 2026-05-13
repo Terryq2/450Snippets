15b39bc08041139503cd64fff65ff6f3e98ca683
Terry Qiu <terryq2@illinois.edu>
2026-05-13 18:14:53 -0500

    NdNewton

--- ConditionNumbers.py
+++ ConditionNumbers.py
@@ -5,6 +5,9 @@ from numpy import linalg as la
 
 
 A = np.array([[3, 1], [0, 0.5]])
+c_1 = np.array([3, 4])
+
+
 
 print(la.cond(A, 1))
 print(la.cond(A, 2))
@@ -12,3 +15,7 @@ print(la.cond(A, np.inf))
 
 
 print(102/12 * (10**(-11)/1.3))
+
+print(la.norm(c_1))
+
+print(2/(2)**(1/2))
--- NewtonMethodTest.py
+++ NewtonMethodTest.py
@@ -1,6 +1,7 @@
 from numpy import polynomial
 import numpy as np
 from scipy import optimize
+from numpy import linalg as la
 
 def f(x):
     return (-1 + x + 0.5*x**3)
@@ -8,3 +9,21 @@ def f(x):
 root = optimize.newton(f, 0.5)
 
 print(root)
+
+def g(x, y):
+    first_component = x+2*y - 2
+    second_component = x**2 +4*y**2 - 4 
+    return np.array([first_component, second_component])
+
+def jacobian(x, y):
+    return np.array([[1, 2], [2*x, 8*y]])
+
+def newton(step_num):
+    x = np.array([1, 2])
+    for k in range (step_num):
+        s = la.solve(jacobian(x[0], x[1]), -1 * g(x[0], x[1]))
+        x = x+ s
+        print("Iteration", k, x)
+    return x
+
+newton(100)
